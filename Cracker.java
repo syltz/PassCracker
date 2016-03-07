6041ac3342ca5cf79f8c1b706f182051348ad1d4
syltz <hagge@HaggesHaxxMaskin.localdomain>
2016-03-07 01:12:33 +0100

    I can't be arsed to do any more work on it atm.

--- Cracker.java
+++ Cracker.java
@@ -2,8 +2,28 @@ import java.util.*;
 import java.security.*;
 import java.io.*;
 
-public class Cracker {
-    public static void main(String[] args) {
-        String 
+public class Cracker  {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String inputHash = null;
+        try {
+            inputHash = args[0];
+        } catch (ArrayIndexOutOfBoundsException noHash) {
+            System.out.println("You have to provide a hash dude"); //Can't really do anything without a hash
+            System.exit(0);
+        }
+        try {
+            File wordlist = new File(args[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            File wordlist = null; //No one actually cares, this just means no wordlist
+        }
+        System.out.println(compareHashToWord(inputHash, "asd"));
+        
+    }
+    public static boolean compareHashToWord(String hash, String word) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] bytesOfWord = word.getBytes();
+        md.update(bytesOfWord);
+        System.out.println("md digest: " + md.digest);
+        return Arrays.equals(hash.getBytes(), md.digest());
     }
 }
